b51a5e2e6c91a8a98b83855bee68cf19c56b6cd9
Edwin Mendoza <MENDOZA_EDWIN@student.smc.edu>
2020-10-06 13:06:36 -0700

    Circles are now inlcuded

--- sketch.js
+++ sketch.js
@@ -1,9 +1,30 @@
+var circleRandom = {
+    xLocation: 0,
+    yLocation: 0
+}
+
+function colorRandomValues(){
+  return Math.floor(Math.random() * 255 ) - 255;
+}
+
+var maxCanvas = {
+    width: 1800,
+    height: 600,
+}
+
 function setup(){
-    createCanvas(1800, 600);
+    frameRate(10);
+    createCanvas(maxCanvas.width, maxCanvas.height);
+    background(0);
 }
 
 function draw(){
 
-     background (100,100,100)
-     circle(mouseX,mouseY,100)
+   
+     circleRandom.xlocation = random(0,maxCanvas.width);
+     circleRandom.yLocation = random(0, maxCanvas.height);
+     fill(colorRandomValues,colorRandomValues,colorRandomValues);
+    noStroke();
+     circle(circleRandom.xlocation,circleRandom.yLocation ,50);
+     
 }
